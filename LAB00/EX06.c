8a95fb162c11f23a600a4f340f7b3822b8734153
JPatthanan <118196644+ItsPatthanan@users.noreply.github.com>
2022-11-16 21:15:55 +0700

    Add files via upload

---
+++ LAB00/EX06.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    int customers;
+do{
+    printf("Search for the target\n");
+    printf("promote products\n");
+    printf("Post a picture of the product on social media\n");
+    printf("hire a reviewer\n");
+    printf("more than 1000 customers per day ?\n");
+    printf("press 1 for yes press 0 fon no\n");
+    scanf("%d", &customers);
+}while(customers == 0);
+    printf("Rich");
+return 0;
+}
