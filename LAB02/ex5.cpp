dedfd665d50103b503c465d2ca07d343c09cf028
JPatthanan <118196644+ItsPatthanan@users.noreply.github.com>
2022-12-14 01:48:08 +0700

    Update ex5.cpp

--- LAB02/ex5.cpp
+++ LAB02/ex5.cpp
@@ -1,16 +1,25 @@
 #include <stdio.h>
-#include <string.h>
 
-char *GetText();
-int main() {
-  char str[16] ;
-  strcpy(str, GetText());
-  printf("\n%s\n", str);
-  return 0;
+char (*GetText2D( int row ))[16];
+
+int main(){
+    char (*str)[16];
+    int row;
+    printf("rows ? : ");
+    scanf("%d", &row);
+    str = GetText2D(row);
+    for ( int i = 0 ; i < row ; i++ ){
+        printf("%s \n", str[i]);
+    } 
+    return 0;
 }
-char *GetText(){
-  char *text = new char[16];
-  printf("Enter String : ");
-  gets(text);
-  return text;
+
+char (*GetText2D( int row ))[16]{
+    int i;
+    char (*text)[16];
+    for ( i = 0 ; i < row ; i++ ){
+    	printf("Enter String [%d] : ", i);
+        scanf("%s", text[i]);
+    } 
+    return text;
 }
