d72377e0de43ba254b94a8d08b05f4d5ac842cd9
JPatthanan <118196644+ItsPatthanan@users.noreply.github.com>
2022-12-14 20:28:33 +0700

    Update ex1.c

--- LAB02/ex1.c
+++ LAB02/ex1.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 int main(){
-    char str1[10];
-    char str1[] = "Patthanan";
-    printf("%s",&str1);
+    char str1[10] = "Patthanan";
+    printf("%s",str1);
     return 0;
 }
